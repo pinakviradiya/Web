40e2dfb98fec834a4fef29df9d68ae831720edc2
pinakviradiya <139138055+pinakviradiya@users.noreply.github.com>
2023-11-06 18:17:22 +0530

    Create program1.cpp

---
+++ List5/program1.cpp
@@ -0,0 +1,37 @@
+//Write a C++ program to demonstrate use of Single Inheritance.
+
+#include <iostream>
+using namespace std;
+
+
+class Shape {
+public:
+    void display() {
+        cout<<"Pinak viradiya"<<endl;
+		cout<<"220130318014"<<endl;
+        cout << "This is a shape." << endl;
+    }
+};
+
+
+class Rectangle : public Shape {
+public:
+    void display() {
+        cout << "This is a rectangle." << endl;
+    }
+
+    void draw() {
+        cout << "Drawing a rectangle." << endl;
+    }
+};
+
+int main() {
+    
+	  Rectangle rect;
+
+      rect.display(); 
+    rect.draw();    
+	
+	  return 0;
+}
+
